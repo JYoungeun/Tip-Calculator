6cc766e0f5eadcef008499785e5ec855cf3977fa
Peter <youngeunpc@yahoo.com>
2024-09-18 10:34:14 -0300

    changes

--- script.js
+++ script.js
@@ -6,4 +6,18 @@ const tipButtonTwo = document.getElementById('tipBtn2');
 const tipButtonThree = document.getElementById('tipBtn3');
 const tipButtonFour = document.getElementById('tipBtn4');
 const tipButtonFive = document.getElementById('tipBtn5');
+const alertText = document.getElementById('alert');
 const resetButton = document.getElementById('resetBtn');
+
+peopleInput.addEventListener('input', () => {
+    if (peopleInput.value === '0') {
+        alertText.classList.remove('hidden');
+        peopleInput.style.borderColor = 'red';
+        peopleInput.style.transition = 'none';
+    } else {
+        alertText.classList.add('hidden');
+        peopleInput.style.borderColor = '';
+    }
+});
+
+const calculateTipAmount = () => {};
